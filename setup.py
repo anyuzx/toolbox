6cb3b2c32b26ade09b295b804b11cd9ba6f29e30
Guang Shi <stefanshi1988@gmail.com>
2025-06-10 22:51:54 -0500

    allow CustomPlot module and misc module to be imported on their own

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy
-
 
 ext_modules = [Extension('_matrixnorm', ['_matrixnorm.pyx'], include_dirs = [numpy.get_include()])]
 
 setup(
-  ext_modules = cythonize(ext_modules),
+    name="toolbox",
+    version="0.1",
+    # build the cython extension
+    ext_modules = cythonize(ext_modules),
+
+    # expose these two modules so `import CustomPlot` and `import misc` work
+    py_modules=[
+        "CustomPlot",
+        "misc",
+    ],
 )
